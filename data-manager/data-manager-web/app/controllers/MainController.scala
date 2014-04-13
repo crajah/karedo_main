747881a53c84492412c4b116978a877912a971c3
galarragas <galarragas@gmail.com>
2014-04-13 17:05:50 +0100

    WEB flow sketched out

--- data-manager/data-manager-web/app/controllers/MainController.scala
+++ data-manager/data-manager-web/app/controllers/MainController.scala
@@ -3,7 +3,14 @@ package controllers
 import play.api.mvc.{Action, Result, Controller}
 
 object MainController extends Controller {
-  def index = Action { Ok(views.html.index.render("Hello from UserProfileWeb")) }
+  def index = Action { Ok(views.html.index.render("Hello from Data Manager Web UI")) }
 
   def register = Action { Ok(views.html.register.render) }
+  def submitRegistration = Action {
+    //generate activation code and send
+
+    Ok(views.html.confirmActivation.render)
+    //implicit request => Redirect(routes.MainController.confirmActivation.absoluteURL(false) )
+  }
+  def confirmActivation = Action { Ok(views.html.registrationCompleted.render) }
 }
