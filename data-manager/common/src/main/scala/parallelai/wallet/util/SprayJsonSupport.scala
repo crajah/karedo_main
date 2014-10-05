3b22b75a5f1cfd28950a4d386dbecdddb62894da
galarragas <galarragas@gmail.com>
2014-10-05 11:27:20 +0100

    Just Formatting

--- data-manager/common/src/main/scala/parallelai/wallet/util/SprayJsonSupport.scala
+++ data-manager/common/src/main/scala/parallelai/wallet/util/SprayJsonSupport.scala
@@ -7,18 +7,23 @@ trait SprayJsonSupport {
   import spray.json._
   import spray.http._
 
-  implicit def sprayJsonUnmarshallerConverter[T](reader: RootJsonReader[T]) =
-    sprayJsonUnmarshaller(reader)
+  implicit def sprayJsonUnmarshallerConverter[T](reader: RootJsonReader[T]) = sprayJsonUnmarshaller(reader)
+
   implicit def sprayJsonUnmarshaller[T: RootJsonReader] = Unmarshaller[T](MediaTypes.`application/json`) {
       case x: HttpEntity.NonEmpty ⇒
         val json = JsonParser(x.asString(defaultCharset = HttpCharsets.`UTF-8`))
         jsonReader[T].read(json)
     }
+
   implicit def sprayJsonMarshallerConverter[T](writer: RootJsonWriter[T])(implicit printer: JsonPrinter = PrettyPrinter) =
     sprayJsonMarshaller[T](writer, printer)
+
   implicit def sprayJsonMarshaller[T](implicit writer: RootJsonWriter[T], printer: JsonPrinter = PrettyPrinter) =
     Marshaller.delegate[T, String](ContentTypes.`application/json`) { value ⇒
       val json = writer.write(value)
+
+      println(s"Json Value is $json")
+
       printer(json)
     }
 }
