41889a125620e2ee696164dcc0da67670d70d133
claudio.pacchiega <claudio.pacchiega@gmail.com>
2015-03-25 01:11:25 +0100

    3 missing files...

---
+++ data-manager/model/src/main/scala/parallelai/wallet/entity/DataTypes2.scala
@@ -0,0 +1,12 @@
+package parallelai.wallet.entity
+
+/**
+ * These are the types we are finding on the DAO level
+ */
+
+import java.util.UUID
+
+import com.novus.salat.annotations.Key
+import org.joda.time.DateTime
+
+case class KaredoSales(@Key("_id") id: UUID = UUID.randomUUID(), userId: UUID, adId: UUID, code: String, date: DateTime=new DateTime())
