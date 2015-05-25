2a5e3e09817d7de8513294f2b7fc962798e0d8d0
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2015-05-26 00:14:21 +0200

    added monad Try checking in MongoMediaDAO to be sure not raising internal exception when asking for an invalid objectid

--- data-manager/model/src/main/scala/parallelai/wallet/persistence/mongodb/MongoMediaDAO.scala
+++ data-manager/model/src/main/scala/parallelai/wallet/persistence/mongodb/MongoMediaDAO.scala
@@ -9,6 +9,9 @@ import parallelai.wallet.persistence.MediaDAO
 import java.io.FileInputStream
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.gridfs.Imports._
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
 
 class MongoMediaDAO(implicit val bindingModule: BindingModule) extends MediaDAO with MongoConnection with Injectable {
 
@@ -29,20 +32,35 @@ class MongoMediaDAO(implicit val bindingModule: BindingModule) extends MediaDAO 
     }
   }
 
+  private def stringAsObjectId(idStr: String) =  new ObjectId(idStr)
+    
   override def findById(id: String): Option[MediaContent] =
-    gridfs.findOne( stringAsObjectId(id) ) map { extractContent }
-
+    {
+      // tries to get object id from id and if fails go in the second lef of match
+      Try(stringAsObjectId(id)).map { 
+        objId => gridfs.findOne(objId).flatMap {
+          dbfs => Some(extractContent(dbfs))
+        }
+      }
+      
+      // check if we succeeded and return last Option
+      match {
+        case Success(x) => x
+        case _ => None
+      }
+    }
 
   override def findByName(name: String): Option[MediaContent] =
     gridfs.findOne(name) map { extractContent }
 
   override def delete(id: String): Unit =
-    gridfs.remove(stringAsObjectId(id))
+    // if stringsucceeded then remove
+    Try(stringAsObjectId(id))
+      .map(gridfs.remove)
+     
 
   override def list: List[MediaContentDescriptor] =
     gridfs.toList.map { extractDescriptor }
-
-  private def stringAsObjectId(idStr: String): ObjectId = new ObjectId(idStr)
 
   private def extractDescriptor(dbFile: GridFSDBFile): MediaContentDescriptor =
     MediaContentDescriptor(name = dbFile.filename.get, contentType = dbFile.contentType.get, id = dbFile._id.get.toHexString)
