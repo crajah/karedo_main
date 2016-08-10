975df4ef8b5737229095f8908331cb8ff292ba39
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-08-10 23:22:47 +0200

    Must open only one connection to MongoDB
    Using an object and getInstance to be sure that we are not taking too
    many connections open with mongoDB

--- data-manager/model/src/main/scala/parallelai/wallet/persistence/mongodb/MongoConnection.scala
+++ data-manager/model/src/main/scala/parallelai/wallet/persistence/mongodb/MongoConnection.scala
@@ -1,13 +1,13 @@
 package parallelai.wallet.persistence.mongodb
 
-import com.mongodb.casbah.{MongoCredential, MongoClient}
+import com.mongodb.casbah.{ MongoCredential, MongoClient }
 import com.mongodb.ServerAddress
 import com.escalatesoft.subcut.inject.Injectable
 
 /**
  * Created by crajah on 06/07/2014.
  */
-trait MongoConnection  {
+trait MongoConnection {
   self: Injectable =>
 
   lazy val mongoHost: String = injectProperty[String]("mongo.server.host")
@@ -15,16 +15,29 @@ trait MongoConnection  {
   lazy val mongoDbName: String = injectProperty[String]("mongo.db.name")
   lazy val mongoDbUser: String = injectProperty[String]("mongo.db.user")
   lazy val mongoDbPwd: String = injectProperty[String]("mongo.db.pwd")
-  lazy val mongoClient = {
-    println(s"*** mongoHost: $mongoHost, mongoPort: $mongoPort")
-    if (mongoDbUser.isEmpty) {
-      MongoClient(mongoHost, mongoPort)
-    } else {
-      MongoClient(new ServerAddress(mongoHost, mongoPort), List(MongoCredential.createMongoCRCredential(mongoDbUser, mongoDbName, mongoDbPwd.toCharArray)))
-    }
-  }
+  lazy val mongoClient = MongoInstance.getInstance(mongoHost,mongoPort,mongoDbName,mongoDbUser,mongoDbPwd)
 
   lazy val db = mongoClient(mongoDbName)
 
+  
 
 }
+object MongoInstance {
+    var instance: Option[MongoClient] = None
+
+    def getInstance(mongoHost: String, mongoPort: Int, mongoDbName: String, mongoDbUser: String, mongoDbPwd: String): MongoClient = {
+      def open = {
+        println(s"*** mongoHost: $mongoHost, mongoPort: $mongoPort")
+        if (mongoDbUser.isEmpty) {
+          MongoClient(mongoHost, mongoPort)
+        } else {
+          MongoClient(new ServerAddress(mongoHost, mongoPort),
+            List(MongoCredential.createMongoCRCredential(mongoDbUser, mongoDbName, mongoDbPwd.toCharArray)))
+        }
+      }
+      if (instance == None) instance = Some(open)
+      instance.get
+    }
+
+  }
+
