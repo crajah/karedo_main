6fbc94d1f4fb8181104905e676f3878ab6890169
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-08-13 17:10:18 +0200

    making unit test working again (/bids is not /auctions) and some serialization done in json to be sure we are using correct rtb serialization for currencies

--- data-manager/data-manager-api/src/test/scala/rtb/RtbAccess.scala
+++ data-manager/data-manager-api/src/test/scala/rtb/RtbAccess.scala
@@ -13,34 +13,92 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
 @RunWith(classOf[JUnitRunner])
-class RtbAccess extends Specification with RtbJsonProtocol {
+object RtbAccess extends Specification with RtbJsonProtocol {
 
-  val apiLocation = "http://rtb.karedo.co.uk:12339"
-  val path = "/bids"
+  val apiLocation = "http://rtb.karedo.co.uk"
+  val port1 = 12339
+  val port2 = 12340
 
-  def str(): String = {
-    val rtb =
-      Rtb(id = 2508744128L, timestamp = "2016-08-10T21:27:29.450Z", url = "http://datacratic.com/", language = "en", exchange = "mock", location = Location(countryCode = "CA", regionCode = "QC", cityName = "Montreal"), userIds = UserId(prov = 1568485738, xchg = 1475615520), imp = List(Impression(id = 1, formats = List("160x600")), Impression(id = 2, formats = List("160x600"))), spots = List(Spot(id = 1, formats = List("160x600"))))
-    val json = rtb.toJson.toString
-    json
+  val path1 = "/auctions"
+  val path2 = "/win"
+
+  def anAuction(): String = {
+
+    Rtb(id = 2508744128L, timestamp = "2016-08-10T21:27:29.450Z", url = "http://datacratic.com/",
+      language = "en", exchange = "mock",
+      location = Location(countryCode = "CA", regionCode = "QC", cityName = "Montreal"),
+      userIds = UserId(prov = "1568485738", xchg = "1475615520"),
+      imp = List(
+        Impression(id = 1, formats = List("160x600")),
+        Impression(id = 2, formats = List("160x600"))
+      ),
+      spots = List(Spot(id = 1, formats = List("160x600")))
+    ).toJson.toString
   }
-  "Test" should {
-    "compose json" in {
-      str must beEqualTo("""{"location":{"regionCode":"QC","cityName":"Montreal","metro":-1,"timezoneOffsetMinutes":-1,"dma":-1,"countryCode":"CA"},"timestamp":"2016-08-10T21:27:29.450Z","url":"http://datacratic.com/","exchange":"mock","id":2508744128,"language":"en","isTest":false,"userIds":{"prov":1568485738,"xchg":1475615520},"imp":[{"id":1,"formats":["160x600"],"position":0},{"id":2,"formats":["160x600"],"position":0}],"spots":[{"id":1,"formats":["160x600"],"position":0}]}""")
+
+  def aWin(): String = {
+    // "account":["hello", "world"],
+    // "adSpotId":"1", "auctionId":"3438835179", "bidTimestamp":0.0, "channels":[], "timestamp":1470865069.551620,
+    // "type":1, "uids":{"prov":"2046608033", "xchg":"678906006"}, "winPrice":[62, "USD/1M"]}
+    Win(account = List("hello", "world"), adSpotId = "1", auctionId = "3438835179", bidTimestamp = 0,
+      channels = List(), timestamp = 1470865069.551620, atype = 1,
+      uids = UserId(prov = "2046608033", xchg = "678906006"),
+      winPrice = RtbCurrency(62, "USD/1M")
+    ).toJson.toString
+  }
+
+  "Rtb layer" should {
+    "* compose json for auction" in {
+      val expected: String =
+        """{"location":{"regionCode":"QC","cityName":"Montreal","metro":-1,
+          |"timezoneOffsetMinutes":-1,"dma":-1,"countryCode":"CA"},
+          |"timestamp":"2016-08-10T21:27:29.450Z","url":"http://datacratic.com/","exchange":"mock","id":2508744128,
+          |"language":"en","isTest":false,
+          |"userIds":{"prov":"1568485738","xchg":"1475615520"},
+          |"imp":[{"id":1,"formats":["160x600"],"position":0},{"id":2,"formats":["160x600"],"position":0}],
+          |"spots":[{"id":1,"formats":["160x600"],"position":0}]}""".stripMargin.replace("\n", "")
+      anAuction() must beEqualTo(
+        expected)
+    }
+    "* compose json for win" in {
+      val s = aWin()
+
+      val expected: String =
+        """{"channels":[],"timestamp":1470865069.55162,"auctionId":"3438835179","adSpotId":"1",
+          |"bidTimestamp":0,"uids":{"prov":"2046608033","xchg":"678906006"},"winPrice":[62,"USD/1M"],
+          |"account":["hello","world"],"type":1}""".stripMargin.replace("\n", "")
+
+      s must beEqualTo(expected)
+
     }
 
 
-    "we can call rtb" in {
-      val request = url(s"$apiLocation$path")
+    "* POST an /auctions" in {
+      val request = url(s"$apiLocation:$port1$path1")
         .POST
-        .setBody(str())
-        .setContentType("application/json","UTF8")
+        .setBody(anAuction())
+        .setContentType("application/json", "UTF8")
+        .setHeader("X-Openrtb-Version", "2.1")
 
       val responseFuture = Http(request)
 
       val response = Await.result(responseFuture, Duration(100, TimeUnit.MILLISECONDS))
+      response.getStatusCode mustEqual 204
+      //response.getResponseBody mustEqual "post called"
+    }
+    "* POST a /win" in {
+      val request = url(s"$apiLocation:$port2$path2")
+        .POST
+        .setBody(aWin())
+        .setContentType("application/json", "UTF8")
+        .setHeader("X-Openrtb-Version", "2.1")
+
+      val responseFuture = Http(request)
+
+      val response = Await.result(responseFuture, Duration(500, TimeUnit.MILLISECONDS))
+      val content = response.getResponseBody
       response.getStatusCode mustEqual 200
-      response.getResponseBody mustEqual "post called"
+
     }
 
   }
--- data-manager/data-manager-data-types/src/main/scala/com/parallelai/wallet/datamanager/data/ApiDataJsonProtocol.scala
+++ data-manager/data-manager-data-types/src/main/scala/com/parallelai/wallet/datamanager/data/ApiDataJsonProtocol.scala
@@ -10,13 +10,14 @@ import spray.http.StatusCodes._
 import spray.json._
 import java.util.UUID
 
-trait ApiDataJsonProtocol extends DefaultJsonProtocol  {
+trait ApiDataJsonProtocol extends DefaultJsonProtocol {
 
   implicit object UuidJsonFormat extends RootJsonFormat[UUID] {
     def write(x: UUID) = JsString(x.toString)
+
     def read(value: JsValue) = value match {
       case JsString(x) => UUID.fromString(x)
-      case x           => deserializationError("Expected UUID as JsString, but got " + x)
+      case x => deserializationError("Expected UUID as JsString, but got " + x)
     }
   }
 
@@ -33,6 +34,24 @@ trait ApiDataJsonProtocol extends DefaultJsonProtocol  {
         case _ => throw new IllegalArgumentException(s"Expected JsString with content having format $DATE_FORMAT for a DateTime attribute value")
       }
     }
+  }
+
+  case class RtbCurrency(value: BigDecimal, name: String)
+  implicit object currencyJson extends JsonFormat[RtbCurrency] {
+    // 1
+    def write(currency: RtbCurrency) = {
+      JsArray(JsNumber(currency.value), JsString(currency.name))
+    }
+    def read(json: JsValue): RtbCurrency = {
+      json match {
+        case JsArray(jsvector) =>
+          val (JsNumber(value),JsString(name)) = (jsvector(0),jsvector(1))
+          RtbCurrency(value,name)
+        case _ => throw new IllegalArgumentException(s"Expected JsVector with content having currency money value and name of currency")
+      }
+    }
+
+
   }
 
   implicit val registrationRequestJson = jsonFormat4(RegistrationRequest)
@@ -73,7 +92,7 @@ trait ApiDataJsonProtocol extends DefaultJsonProtocol  {
   implicit val imageIdJson = jsonFormat1(ImageId)
 
   implicit val statusJson = jsonFormat1(StatusResponse)
-  implicit val summaryImageApiJson= jsonFormat2(SummaryImageApi)
+  implicit val summaryImageApiJson = jsonFormat2(SummaryImageApi)
   implicit val advDetailResponseJson = jsonFormat9(AdvertDetailResponse)
   implicit val advListResponseJson = jsonFormat3(AdvertDetailListResponse)
   implicit val advSummaryResponseJson = jsonFormat6(AdvertSummaryResponse)
@@ -101,10 +120,13 @@ object ApiDataJsonProtocol extends ApiDataJsonProtocol
 trait RtbJsonProtocol extends ApiDataJsonProtocol {
 
   case class Location(countryCode: String, regionCode: String, cityName: String, dma: Int = -1, metro: Int = -1, timezoneOffsetMinutes: Int = -1)
-  case class UserId(prov: Int, xchg: Int)
+
+  case class UserId(prov: String, xchg: String)
 
   case class Impression(id: Int, formats: List[String], position: Int = 0)
+
   case class Spot(id: Int, formats: List[String], position: Int = 0)
+
   case class Rtb(id: Long, timestamp: String, isTest: Boolean = false, url: String, language: String, exchange: String,
                  location: Location, userIds: UserId, imp: List[Impression], spots: List[Spot])
 
@@ -114,5 +136,13 @@ trait RtbJsonProtocol extends ApiDataJsonProtocol {
   implicit val impressionJson = jsonFormat3(Impression)
   implicit val rtbJson = jsonFormat10(Rtb)
 
+  // "account":["hello", "world"],
+  // "adSpotId":"1", "auctionId":"3438835179", "bidTimestamp":0.0, "channels":[], "timestamp":1470865069.551620,
+  // "type":1, "uids":{"prov":"2046608033", "xchg":"678906006"}, "winPrice":[62, "USD/1M"]}
+  
+  case class Win(account: List[String], adSpotId: String, auctionId: String, bidTimestamp: BigDecimal,
+                 channels: List[String], timestamp: Double, atype: Int, uids: UserId, winPrice: RtbCurrency)
+
+  implicit val winJson = jsonFormat(Win, "account", "adSpotId", "auctionId", "bidTimestamp", "channels", "timestamp", "type", "uids", "winPrice")
 
 }
