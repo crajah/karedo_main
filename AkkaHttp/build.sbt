763fe5cfd7f0e1feaa7e8ffddef9e94ac3e360b2
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-08-14 01:41:21 +0200

    akka-http project with some sandboxing tests to see if we can migrate to akka-http safely

--- AkkaHttp/build.sbt
+++ AkkaHttp/build.sbt
@@ -7,15 +7,17 @@ scalaVersion := "2.11.8"
 scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
 
 libraryDependencies ++= {
-  val akkaV       = "2.4.3"
-  val scalaTestV  = "2.2.6"
+  val akkaV       = "2.4.3" // akka 2.4 is supporting akkahttp
+  val scalaTestV  = "3.0.0"
   Seq(
-    "com.typesafe.akka" %% "akka-actor" % akkaV,
+    /*"com.typesafe.akka" %% "akka-actor" % akkaV,
     "com.typesafe.akka" %% "akka-stream" % akkaV,
+    "com.typesafe.akka" %% "akka-http-testkit" % akkaV,*/
+
     "com.typesafe.akka" %% "akka-http-experimental" % akkaV,
     "com.typesafe.akka" %% "akka-http-spray-json-experimental" % akkaV,
-    "com.typesafe.akka" %% "akka-http-testkit" % akkaV,
-    "org.scalatest"     %% "scalatest" % scalaTestV % "test"
+    "org.specs2" %% "specs2-core" % "3.8.4" % "test",
+    "net.databinder.dispatch" %% "dispatch-core" % "0.11.3" % "test" // very simple http client
   )
 }
     
---
+++ AkkaHttp/src/test/scala/WebService.scala
@@ -0,0 +1,49 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.{Directives, _}
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+
+/**
+  * Created by gerrit on 8/14/16.
+  */
+trait WebService extends Directives {
+
+  val host = "localhost"
+  val port = 8080
+
+  def questionRoutes: Route =  {
+
+    path("question") {
+      get {
+        complete("answer")
+      }
+    } ~
+    path("post") {
+
+      post {
+        handleWith {
+          input: String =>
+            s"answer to $input"
+        }
+      }
+    }
+  }
+
+  implicit val system = ActorSystem("akka-http")
+  implicit val materializer = ActorMaterializer()
+
+  implicit val executionContext = system.dispatcher
+  implicit val timeout = Timeout(10 seconds)
+
+
+  val api = questionRoutes
+  Http().bindAndHandle(handler = api, interface = host, port = port) map { binding =>
+    println(s"REST interface bound to ${binding.localAddress}")
+  } recover { case ex =>
+    println(s"REST interface could not bind to $host:$port", ex.getMessage)
+  }
+
+}
