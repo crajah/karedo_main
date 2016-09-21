ad8d540ae87865f830ec1f6b06afa8a86468cd71
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-09-21 02:12:09 +0200

    EmailTrait extracted so to prepare for unittesting emailing

--- data-manager/data-manager-api/src/main/scala/core/email.scala
+++ data-manager/data-manager-api/src/main/scala/core/email.scala
@@ -2,11 +2,12 @@ package core
 
 import java.io.IOException
 
-import akka.actor.{Props, ActorLogging, ActorRefFactory, Actor}
-import com.escalatesoft.subcut.inject.{Injectable, BindingModule}
+import akka.actor.{Actor, ActorLogging, ActorRefFactory, Props}
+import com.escalatesoft.subcut.inject.{BindingModule, Injectable}
 import core.EmailActor.SendEmail
+import org.slf4j.LoggerFactory
 import spray.client.pipelining._
-import spray.http.{HttpResponse, FormData, BasicHttpCredentials}
+import spray.http.{BasicHttpCredentials, FormData, HttpResponse}
 
 import scala.concurrent.Future
 
@@ -32,32 +33,19 @@ object DummyEmailActor {
   def props(implicit bindingModule: BindingModule) = Props(classOf[DummyEmailActor], bindingModule)
 }
 
-class EmailActor(implicit val bindingModule: BindingModule) extends Actor with ActorLogging with Injectable {
+trait EmailTrait {
+  val userKey: String
+  val prefix: String
+  val serverEndpoint: String
+  val from : String
+  val logger = LoggerFactory.getLogger(classOf[EmailTrait])
 
-  val userKey = injectProperty[String]("notification.email.auth.user.key")
-  val prefix = injectProperty[String]("notification.email.auth.user.prefix") orElse "key-"
-  val serverEndpoint = injectProperty[String]("notification.email.server.endpoint")
-  val from = injectProperty[String]("notification.email.sender")
-  log.info(s"[T:${Thread.currentThread().getId}]  instantiating email with '$serverEndpoint'")
-
-  import context.dispatcher
-
-  val requestPipeline = addCredentials(BasicHttpCredentials("api", s"$prefix$userKey")) ~> sendReceive
-
-  def receive: Receive = {
-
-    case request@SendEmail(to, body, subject, retryCount) => sendEmail(to, body, subject) recover {
-      case exception if (retryCount > 0) =>
-        log.debug("Failed to send message {} because of exception, retrying", request, exception)
-        self ! SendEmail(to, body, subject, retryCount - 1)
-      case exception =>
-        log.error("Unable to send message {}, exception {}", request, exception)
-    }
-  }
 
   def sendEmail(to: String, body: String, subject: String)(implicit actorRefFactory: ActorRefFactory): Future[Unit] = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val requestPipeline = addCredentials(BasicHttpCredentials("api", s"$prefix$userKey")) ~> sendReceive
 
-    log.info(s"[EMAIL] Sending email endpoint: $serverEndpoint from $from to $to subject $subject")
+    logger.info(s"[EMAIL] Sending email endpoint: $serverEndpoint from $from to $to subject $subject")
     requestPipeline {
       Post(
         serverEndpoint,
@@ -72,12 +60,37 @@ class EmailActor(implicit val bindingModule: BindingModule) extends Actor with A
       )
     } map { httpResponse: HttpResponse =>
       if (httpResponse.status.isFailure) {
-        log.info(s"[EMAIL] Got an error response is ${httpResponse.entity.asString}")
+        logger.info(s"[EMAIL] Got an error response is ${httpResponse.entity.asString}")
         throw new IOException(s"Request failed for reason ${httpResponse.status.value}:${httpResponse.status.defaultMessage}")
       } else {
         ()
       }
     }
   }
+}
+
+class EmailActor(implicit val bindingModule: BindingModule) extends Actor with ActorLogging with Injectable with EmailTrait {
+
+  override val userKey = injectProperty[String]("notification.email.auth.user.key")
+  override val prefix = injectProperty[String]("notification.email.auth.user.prefix")
+  override val serverEndpoint = injectProperty[String]("notification.email.server.endpoint")
+  override val from = injectProperty[String]("notification.email.sender")
+  log.info(s"[T:${Thread.currentThread().getId}]  instantiating email with '$serverEndpoint'")
+
+  import context.dispatcher
+
+
+  def receive: Receive = {
+
+    case request@SendEmail(to, body, subject, retryCount) => sendEmail(to, body, subject) recover {
+      case exception if (retryCount > 0) =>
+        log.debug("Failed to send message {} because of exception, retrying", request, exception)
+        self ! SendEmail(to, body, subject, retryCount - 1)
+      case exception =>
+        log.error("Unable to send message {}, exception {}", request, exception)
+    }
+  }
+
+
 
 }
