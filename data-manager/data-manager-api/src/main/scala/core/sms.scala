8b295305c0af70531c067f49f3fbd943dccb2d43
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-09-21 13:03:41 +0200

    sms testing in local

--- data-manager/data-manager-api/src/main/scala/core/sms.scala
+++ data-manager/data-manager-api/src/main/scala/core/sms.scala
@@ -2,13 +2,13 @@ package core
 
 import java.io.IOException
 
-import akka.actor.{ ActorLogging, Props, Actor }
-import com.escalatesoft.subcut.inject.{ Injectable, BindingModule }
+import akka.actor.{Actor, ActorLogging, ActorRefFactory, Props}
+import com.escalatesoft.subcut.inject.{BindingModule, Injectable}
+import org.slf4j.LoggerFactory
 import spray.client.pipelining._
 import spray.http.Uri.Query
-
 import spray.http._
-import spray.json.DefaultJsonProtocol._ // marshallers
+import spray.json.DefaultJsonProtocol._
 import spray.httpx.SprayJsonSupport._
 
 import scala.concurrent.Future
@@ -52,21 +52,64 @@ class TextmarketerSMSActor(implicit val bindingModule: BindingModule) extends Ac
   def receive: Receive = ???
 }
 
+trait SMSTrait {
+  val accessKey: String
+  val serverEndpoint: String
+  val from: String
+  val logger = LoggerFactory.getLogger(classOf[SMSTrait])
 
-class SMSActor(implicit val bindingModule: BindingModule) extends Actor with ActorLogging with Injectable {
-  val accessKey = injectProperty[String]("notification.sms.auth.accesskey")
-  val serverEndpoint = injectProperty[String]("notification.sms.server.endpoint")
-  val from = injectProperty[String]("notification.sms.sender")
+  case class SMSRequest(recipients: String, originator: String, body: String)
+
+  def sendSMS(to: String, body: String)(implicit actorRefFactory: ActorRefFactory): Future[Unit] = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    // val pipeline: SendReceive = sendReceive
+    val pipeline: HttpRequest => Future[HttpResponse] = {
+      addHeader("Authorization", s"AccessKey $accessKey") ~>
+        sendReceive //~> unmarshal[String]
+    }
+    implicit val jsonSMSRequest = jsonFormat3(SMSRequest)
+
+    val msisdn: String = normaliseMsisdn(to)
+    logger.info(s"[SMS] Sending sms serverEndpoint: $serverEndpoint from: $from to: $msisdn body: $body")
+    pipeline {
+      Post(
+        Uri(serverEndpoint), SMSRequest(msisdn, from, body))
+    } map { httpResponse: HttpResponse =>
+      if (httpResponse.status.isFailure) {
+        val err = s"Request failed for reason ${httpResponse.status.value}:${httpResponse.status.defaultMessage}"
+        logger.error(err)
+        throw new IOException(err)
+
+      } else {
+        logger.error(s"[SMS] Sent a sms, response from service is $httpResponse")
+      }
+    }
+
+  }
+
+}
+object testSMS extends App {
+  val it = new SMSTrait {
+    override val from: String = "Karedo"
+    override val serverEndpoint: String = "https://rest.messagebird.com/messages"
+    override val accessKey: String = "live_RmtS0fUQafb7ae0XK21PVmIpt"
+  }
+  import akka.actor.ActorSystem
+
+  implicit val system = ActorSystem("demo")
+  it.sendSMS("00393319345235","text of message")
+}
+
+class SMSActor(implicit val bindingModule: BindingModule) extends Actor with SMSTrait with ActorLogging with Injectable {
+  override val accessKey = injectProperty[String]("notification.sms.auth.accesskey")
+  override val serverEndpoint = injectProperty[String]("notification.sms.server.endpoint")
+  override val from = injectProperty[String]("notification.sms.sender")
 
   log.info(s"[SMS] [T:${Thread.currentThread().getId}]  instantiating SMS with '$serverEndpoint' and accessKey '${accessKey}'")
 
   import context.dispatcher
 
-  // val pipeline: SendReceive = sendReceive
-  val pipeline: HttpRequest => Future[HttpResponse] = {
-    addHeader("Authorization", s"AccessKey $accessKey") ~>
-      sendReceive //~> unmarshal[String]
-  }
+
 
   def receive: Receive = {
     case request @ SendSMS(number, body, retryCount) => sendSMS(number, body) recover {
@@ -77,24 +120,5 @@ class SMSActor(implicit val bindingModule: BindingModule) extends Actor with Act
         log.error("Unable to send message {}, exception {}", request, exception)
     }
   }
-  case class SMSRequest(recipients: String, originator: String, body: String)
 
-  def sendSMS(to: String, body: String): Future[Unit] = {
-    implicit val jsonSMSRequest = jsonFormat3(SMSRequest)
-
-    val msisdn: String = normaliseMsisdn(to)
-    log.debug(s"[SMS] Sending sms serverEndpoint: $serverEndpoint from: $from to: $msisdn body: $body")
-    pipeline {
-      Post(
-        Uri(serverEndpoint), SMSRequest(msisdn, from, body))
-    } map { httpResponse: HttpResponse =>
-      if (httpResponse.status.isFailure) {
-        throw new IOException(s"Request failed for reason ${httpResponse.status.value}:${httpResponse.status.defaultMessage}")
-
-      } else {
-        log.error(s"[SMS] Sent a sms, response from service is $httpResponse")
-      }
-    }
-
-  }
 }
