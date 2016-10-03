6623dca728296c32643b146484f4cc2a98438131
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-10-04 01:14:26 +0200

    some small refactoring

--- karedo_api/src/main/scala/sms.scala
+++ karedo_api/src/main/scala/sms.scala
@@ -1,20 +1,6 @@
-import java.io.IOException
-
-import akka.actor.{Actor, ActorLogging, ActorRefFactory, ActorSystem, Props}
-import akka.http.javadsl.model.headers.CustomHeader
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.{Http, model}
-import akka.http.scaladsl.model.{HttpHeader, HttpRequest, HttpResponse, Uri}
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.stream.ActorMaterializer
 import karedo.entity.dao.Configurable
-import org.slf4j.LoggerFactory
-import akka.http.scaladsl.model.HttpMethods._
-import akka.http.scaladsl.model.headers.RawHeader
-import spray.json.DefaultJsonProtocol._
-
-import scala.concurrent.Future
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.stream.{ActorMaterializer, Materializer}
-import spray.json.DefaultJsonProtocol
 
 
 object SMSActor {
@@ -38,71 +24,8 @@ object SMSActor {
 
 import SMSActor._
 
-// used for tests
-class DummySMSActor extends Configurable
-  with Actor
-  with ActorLogging {
 
-  log.info(s"[DUMMYSMS] [T:${Thread.currentThread().getId}]  instantiating DUMMY SMSActor")
-  def receive: Receive = {
-    case request @ SendSMS(number, body, retryCount) =>
-      log.info(s"[DUMMYSMS] (sms to: $number, body: $body)")
-  }
-}
-object DummySMSActor {
-  def props = Props(new DummySMSActor)
-}
 
-// no need to specify anymore Textmarketer but in case we have the placeholder for additional services
-class TextmarketerSMSActor extends Configurable with Actor with ActorLogging {
-  def receive: Receive = ???
-}
-
-trait SMSTrait extends SprayJsonSupport with DefaultJsonProtocol {
-  val accessKey: String
-  val serverEndpoint: String
-  val from: String
-  implicit val system: ActorSystem
-  implicit val materializer: Materializer
-  val logger = LoggerFactory.getLogger(classOf[SMSTrait])
-
-  case class SMSRequest(recipients: String, originator: String, body: String)
-
-  def sendSMS(to: String, body: String)(implicit actorRefFactory: ActorRefFactory): Future[Unit] = {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    implicit val jsonSMSRequest = jsonFormat3(SMSRequest)
-
-    val msisdn: String = normaliseMsisdn(to)
-    logger.info(s"[SMS] Sending sms serverEndpoint: $serverEndpoint from: $from to: $msisdn body: $body")
-
-    Http().singleRequest(HttpRequest(POST,Uri(serverEndpoint),entity=SMSRequest(msisdn, from, body).toJson.toString).
-      addHeader(RawHeader("Authorization", s"AccessKey $accessKey"))).map { httpResponse: HttpResponse =>
-      if (httpResponse.status.isFailure) {
-        val err = s"Request failed for reason ${httpResponse.status.value}:${httpResponse.status.defaultMessage}"
-        logger.error(err)
-        throw new IOException(err)
-
-      } else {
-        logger.info(s"[SMS] Sent a sms, response from service is $httpResponse")
-      }
-    }
-
-  }
-
-}
-object testSMS extends App {
-  implicit val system = ActorSystem("demo")
-  val it = new SMSTrait {
-    override val from: String = "Karedo"
-    override val serverEndpoint: String = "https://rest.messagebird.com/messages"
-    override val accessKey: String = "live_RmtS0fUQafb7ae0XK21PVmIpt"
-    override implicit val system = ActorSystem("testSMS")
-    override val materializer = ActorMaterializer()
-  }
-
-  it.sendSMS("00393319345235","text of message 2")
-}
 
 class SMSActor
   extends Configurable
