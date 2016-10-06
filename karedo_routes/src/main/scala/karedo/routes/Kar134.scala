81cf91103a3320dcb4fd8ac95b27047045ee4f89
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-10-06 17:08:45 +0200

    writing some residual parts of the /account/xxxx/ads with the various cases ...

--- karedo_routes/src/main/scala/karedo/routes/Kar134.scala
+++ karedo_routes/src/main/scala/karedo/routes/Kar134.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import karedo.entity.dao.{KO, OK, Result}
-import karedo.entity.{DbUserAccount, DbUserApp, UserAccount, UserApp}
+import karedo.entity._
 import org.slf4j.LoggerFactory
 import spray.json._
 import DefaultJsonProtocol._
@@ -20,12 +20,9 @@ trait Kar134 extends KaredoRoute {
 
   val dbUserApp = new DbUserApp {}
   val dbUserAccount = new DbUserAccount {}
+  val dbUserSession = new DbUserSession {}
   //case class Ad(url:String)
   //case class AdsReturned(List[Ad]=List())
-
-  def getAdsFor(accountId: UUID): Result[String, String] = {
-    OK(List("a", "b").toJson.toString)
-  }
 
 
   // exec will be moved to proper actor (or stream in business logic layer)
@@ -41,63 +38,24 @@ trait Kar134 extends KaredoRoute {
     if (accountId == "0") {
       anonymousCall(applicationId)
     }
-
-
-    else KO(Error("Not yet implemented"))
-
-
-    //    } else {
-    //      if UserApp.find( application_id ) != null {
-    //        // Account mapped to application
-    //
-    //        if UserApp.find( application_id ) != account_id {
-    //          // Account in Application is different to what is stored on device.
-    //          // Should fix itself at Login
-    //          local_account = UserApp.get(application_id)
-    //          local_account_id = local_account.account_id
-    //
-    //          using local_account_id => get ads
-    //
-    //          return 205 Reset Content + ads
-    //          /* Mobile App: 205 indicates old data. Reset it and Mark for 4b. Send Code screen */
-    //
-    //        } else {
-    //          // The right user to application mapping
-    //          local_account_id = UserApp.get( application_id )
-    //          local_account = UserAccount.get( local_account_id )
-    //
-    //          if session_id == null OR UserSession.find( session_id ) == null AND UserSession.find( session_id ) != local_account_id {
-    //            // User has never logged in
-    //
-    //            using local_account => get ads
-    //
-    //            return 206 Partial Content + ads + account_id
-    //            /* Mobile App: 206 indicates user login has expired. Mark for 4a. Login screen */
-    //
-    //          } else {
-    //            // User has never logged in
-    //            using local_account_id => get ads
-    //
-    //            return 200 OK + ads + account_id
-    //            /* Mobile App: 200 indicates everything is fine and the user is logged in */
-    //          }
-    //        }
-    //      } else {
-    //        // Account not mapped to Application
-    //        // This is temporary account. Login will fix it.
-    //        local_account = create new UserAccount()
-    //        local_account_id = local_account.account_id
-    //        UserApp.put( application_id, account_id )
-    //
-    //        return 201 Created + ads
-    //        /* Mobile App: 201 indicates first time user. Mark for 4b. Send Code screen */
-    //      }
-    //    }
-
-
+    else {
+      accountProvided(accountId, applicationId, sessionId)
+    }
   }
 
+  def getAds(uapp: Result[String, UserApp], code: Int): Result[Error, APIResponse] = {
+    if (uapp.isOK) {
+      val app = uapp.get
+      val uAds = getAdsFor(app.account_id)
+      if (uAds.isOK) {
+        OK(APIResponse(uAds.get.toString, code))
+      } else KO(Error(s"Can't get ads because of ${uAds.err}"))
+    } else KO(Error(s"application cant be found because of ${uapp.err}"))
+  }
 
+  def getAdsFor(accountId: String): Result[String, String] = {
+    OK(List("a", "b").toJson.toString)
+  }
 
 
   def anonymousCall(applicationId: String): Result[Error, APIResponse] = {
@@ -122,16 +80,35 @@ trait Kar134 extends KaredoRoute {
 
   }
 
-  def getAds(uapp: Result[String, UserApp], code: Int): Result[Error, APIResponse] = {
+  def accountProvided(accountId: String, applicationId: String, sessionId: Option[String]):
+    Result[Error, APIResponse] = {
+    val uapp = dbUserApp.getById(applicationId)
     if (uapp.isOK) {
-      val app = uapp.get
-      val uAds = getAdsFor(app.account_id)
-      if (uAds.isOK) {
-        OK(APIResponse(uAds.get.toString, code))
-      } else KO(Error(s"Can't get ads because of ${uAds.err}"))
-    } else KO(Error(s"application cant be found because of ${uapp.err}"))
+      val storedAccountId = uapp.get.account_id
+      if (accountId != storedAccountId) {
+        getAds(uapp, 205)
+      } else {
+        val uacc = dbUserAccount.getById(accountId)
+        if (sessionId.isDefined) {
+          if (dbUserSession.getById(sessionId.get).isOK) {
+            getAds(uapp, 200)
+          } else {
+            /* Mobile App: 206 indicates user login has expired. Mark for 4a. Login screen */
+            getAds(uapp, 206)
+          }
+        } else {
+          getAds(uapp, 206)
+        }
+      }
+    } else {
+      val uacc = dbUserAccount.insertNew(UserAccount())
+      val uapp = dbUserApp.insertNew(UserApp(applicationId, uacc.get.id))
+      if (uapp.isKO) {
+        logger.error(s"can't insert mapping")
+      }
+      getAds(uapp, 201)
+    }
   }
-
 
   def kar134 = {
     Route {
@@ -141,17 +118,17 @@ trait Kar134 extends KaredoRoute {
 
       path("account" / Segment / "ads") {
         accountId =>
-          optionalHeaderValueByName("X_Identification") { deviceId =>
-            get {
-              parameters('p, 's ?, 'c ?) {
-                (applicationId, sessionId, adCount) =>
-                  doCall(
-                    {
+          optionalHeaderValueByName("X_Identification") {
+            deviceId =>
+              get {
+                parameters('p, 's ?, 'c ?) {
+                  (applicationId, sessionId, adCount) =>
+                    doCall({
                       exec(accountId, deviceId, applicationId, sessionId, adCount)
                     }
-                  )
+                    )
+                }
               }
-            }
           }
       }
 
