7ee85394023fcd5c03b8091e2c67c5b5430c6ab2
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2016-10-09 02:31:30 +0200

    implemented ads to the best I could understand from the documentation

--- karedo_persist/src/main/scala/karedo/entity/DbUserAd.scala
+++ karedo_persist/src/main/scala/karedo/entity/DbUserAd.scala
@@ -1,5 +1,7 @@
 package karedo.entity
 
+import java.util.UUID
+
 import com.mongodb.casbah.commons.MongoDBObject
 import karedo.entity.dao.{DbMongoDAO, Keyable}
 import org.joda.time.DateTime
@@ -12,17 +14,47 @@ case class Channel
   , channel_id: String
 )
 
+case class Beacon (
+  beacon: String
+)
+
+
+case class ImageAd
+(
+  image_url: String
+  , click_url: String = ""
+  , h: Int = 0
+  , w: Int = 0
+  , beacons: List[Beacon] = List()
+)
+case class VideoAd
+(
+  video_url: String = ""
+  , duration: String = ""
+  , click_url: String = ""
+  , h: Int = 0
+  , w: Int = 0
+  , beacons: List[Beacon] = List()
+)
+
 case class UserAd
 (
-  // this is the univoque AdId
-  @Key("_id") id: String
-  , account_id: String
-  , adType: String = ""
-  , imp_url: String
-  , click_url: String
-  , addomain: String
-  , cid: String
-  , crid: String
+  // this is the univoque created by MongoDB
+  @Key("_id") id: String = UUID.randomUUID().toString
+  , sort: Int = 0 // possibly used to sort this
+  , ad_id: String // assigned by Rtb?
+  , application_id: String // primary key for extracting ads for this user
+  , ad_type: String = "" // VIDEO/IMAGE/NATIVE
+  , adm: String = "" // Ad Markup
+  , image_ad: Option[ImageAd] = None
+  , video_ad: Option[VideoAd] = None
+  , addomain: String = ""
+  , iurl: String = ""
+  , nurl: String = ""
+  , cid: String = ""
+  , crid: String = ""
+  , w: Int = 0
+  , h: Int = 0
   , channels: List[Channel] = List()
   , ts: DateTime = now
 
@@ -31,25 +63,28 @@ case class UserAd
 // add implementation if you need special functionalities
 trait DbUserAd extends DbMongoDAO[String, UserAd] {
   // preloads some values associated to accountId: accountid
-  def preload() = {
+  def preload(applId: String, num: Int) = {
 
     def add(index: Int) = {
-      val applId = "applId"
-      val accountId = "acctId"
-      val inserted = insertNew(UserAd(id = index.toString, accountId, applId, imp_url = s"http://www.ad.com/?id=$index",
-        addomain = "domain.com", click_url = "http://www.ad.com/click/?c=45", cid = "cid", crid = "crid"
+
+      val image=ImageAd(image_url = s"imageurl $index")
+      val video=VideoAd(video_url = s"videourl $index")
+      val inserted = insertNew(
+        UserAd(ad_id = index.toString,
+        application_id = applId,
+        addomain = "domain.com", cid = "cid", crid = "crid"
         ,channels = List(Channel("channel","CH01"), Channel("channel2","CH02"))
       ))
     }
 
     deleteAll()
-    for (i <- 1 to 100) add(i)
-    100
+    for (i <- 1 to num) add(i)
+    10
 
   }
 
-  def getAdsForUser(accountId: String) = {
-    dao.find(MongoDBObject("account_id" -> accountId)).sort(orderBy = MongoDBObject("sort" -> 1)).toList
+  def getAdsForApplication(applicationId: String) = {
+    dao.find(MongoDBObject("application_id" -> applicationId)).toList
   }
 }
 
