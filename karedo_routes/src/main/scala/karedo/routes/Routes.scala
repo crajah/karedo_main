ec73724f584152a878a93530e9c0dc91d8e664ca
Chandan Rajah <crajah@parallelai.com>
2016-10-16 16:14:09 +0100

    Reverting Reflect for Route add. needs more testing

--- karedo_routes/src/main/scala/karedo/routes/Routes.scala
+++ karedo_routes/src/main/scala/karedo/routes/Routes.scala
@@ -12,24 +12,37 @@ trait Routes
 
     with RouteDebug {
 
-  override val routes = {
-    println("findAllRoutesExtendingKaredoRoute")
-    def companion[T](name : String)(implicit man: Manifest[T]) : T =
-      Class.forName(name).getField("MODULE$").get(null).asInstanceOf[T]
+  override val routes = Kar134.route ~ Kar135.route ~ Kar136.route ~ Kar166.route ~
+    Kar169.route ~ Kar170.route ~ Kar171.route ~ Kar172.route ~ Kar188.route ~ Kar189.route ~
+    Kar194.route ~ Kar195.route 
 
-    import org.clapper.classutil.ClassFinder
-    val rootRoute: Route = path("") {
-      get(complete("Karedo API version 0.0.2-SNAPSHOT"))
-    }
-    val classes = ClassFinder().getClasses()
-    val classesInfos = ClassFinder.concreteSubclasses("karedo.routes.KaredoRoute",classes)
-    val routes =  classesInfos.toList map { x:ClassInfo =>
-      val obj:KaredoRoute = companion[KaredoRoute](x.name)
-      obj.route
-    }
-    routes.foldLeft(rootRoute)(_~_)
-
-  }
+//  override val routes = {
+//    println("findAllRoutesExtendingKaredoRoute")
+//    def companion[T](name : String)(implicit man: Manifest[T]) : T =
+//      Class.forName(name).getField("MODULE$").get(null).asInstanceOf[T]
+//
+//    import org.clapper.classutil.ClassFinder
+//    val rootRoute: Route = path("") {
+//      get(complete("Karedo API version 0.0.2-SNAPSHOT"))
+//    }
+//    val finder = ClassFinder()
+//    val classes = finder.getClasses()
+//
+//    val classesInfos = ClassFinder.concreteSubclasses("karedo.routes.KaredoRoute",classes)
+//
+//    classesInfos.foreach(println)
+//
+//    val routes =  classesInfos.toList map { x:ClassInfo =>
+//      val obj:KaredoRoute = companion[KaredoRoute](x.name)
+//      obj.route
+//    }
+//    val newRoutes = routes.foldLeft(rootRoute)(_~_)
+//
+//    println(newRoutes)
+//
+//    newRoutes
+//
+//  }
 
   override val logger = LoggerFactory.getLogger(classOf[Routes])
 
