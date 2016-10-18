81ce8c4a717a4c9a95c5e35115f7dc2112602fea
Chandan Rajah <crajah@parallelai.com>
2016-10-18 23:48:21 +0100

    Added script to build and run the backend.

---
+++ build_and_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
+git pull
+
+sbt clean publish-local routes:assembly
+
+nohup java -jar karedo_routes/target/scala-2.11/routes-assembly-0.0.2-SNAPSHOT.jar &
+
