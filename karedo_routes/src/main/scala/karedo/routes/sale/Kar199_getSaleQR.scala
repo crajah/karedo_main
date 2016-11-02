2c52a13c22610808a2c3d31f383a7a23b06640ca
Chandan Rajah <crajah@parallelai.com>
2016-11-02 00:38:03 +0000

    Added bug fixes

--- karedo_routes/src/main/scala/karedo/routes/sale/Kar199_getSaleQR.scala
+++ karedo_routes/src/main/scala/karedo/routes/sale/Kar199_getSaleQR.scala
@@ -65,46 +65,46 @@ object Kar199_postQR extends KaredoRoute
     }
   }
 
-  def routeNew =
-    Route {
-      path("qr") {
-        entity(as[Multipart.FormData]) { formData =>
-
-          // collect all parts of the multipart as it arrives into a map
-          val allPartsF: Future[Map[String, Any]] = formData.parts.mapAsync[(String, Any)](1) {
-
-            case b: BodyPart if b.name == "file" =>
-              // stream into a file as the chunks of it arrives and return a future
-              // file to where it got stored
-              val file = File.createTempFile("upload", "tmp")
-              b.entity.dataBytes.runWith(FileIO.toFile(file)).map(_ =>
-                (b.name -> file))
-
-            case b: BodyPart =>
-              // collect form field values
-              b.toStrict(2.seconds).map(strict =>
-                (b.name -> strict.entity.data.utf8String))
-
-          }.runFold(Map.empty[String, Any])((map, tuple) => map + tuple)
-
-//          val done = allPartsF.map { allParts =>
-//            // You would have some better validation/unmarshalling here
+//  def routeNew =
+//    Route {
+//      path("qr") {
+//        entity(as[Multipart.FormData]) { formData =>
+//
+//          // collect all parts of the multipart as it arrives into a map
+//          val allPartsF: Future[Map[String, Any]] = formData.parts.mapAsync[(String, Any)](1) {
+//
+//            case b: BodyPart if b.name == "file" =>
+//              // stream into a file as the chunks of it arrives and return a future
+//              // file to where it got stored
+//              val file = File.createTempFile("upload", "tmp")
+//              b.entity.dataBytes.runWith(FileIO.toFile(file)).map(_ =>
+//                (b.name -> file))
+//
+//            case b: BodyPart =>
+//              // collect form field values
+//              b.toStrict(2.seconds).map(strict =>
+//                (b.name -> strict.entity.data.utf8String))
+//
+//          }.runFold(Map.empty[String, Any])((map, tuple) => map + tuple)
+//
+////          val done = allPartsF.map { allParts =>
+////            // You would have some better validation/unmarshalling here
+////            doCall({
+////              exec(allParts("file").asInstanceOf[File])
+////            })
+////            db.create(Video(
+////              file = allParts("file").asInstanceOf[File],
+////              title = allParts("title").asInstanceOf[String],
+////              author = allParts("author").asInstanceOf[String]))
+////          }
+//
+//          // when processing have finished create a response for the user
+//          onSuccess(allPartsF) { allParts =>
 //            doCall({
 //              exec(allParts("file").asInstanceOf[File])
 //            })
-//            db.create(Video(
-//              file = allParts("file").asInstanceOf[File],
-//              title = allParts("title").asInstanceOf[String],
-//              author = allParts("author").asInstanceOf[String]))
 //          }
-
-          // when processing have finished create a response for the user
-          onSuccess(allPartsF) { allParts =>
-            doCall({
-              exec(allParts("file").asInstanceOf[File])
-            })
-          }
-        }
-      }
-    }
-}
+//        }
+//      }
+//    }
+//}
