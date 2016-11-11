9de1310e27986c8df34bc26cb1875f87944eb7e6
Chandan Rajah <crajah@parallelai.com>
2016-11-11 15:12:00 +0000

    Added some new ads with text

--- karedo_rtb/src/main/scala/karedo/rtb/actor/AdActor.scala
+++ karedo_rtb/src/main/scala/karedo/rtb/actor/AdActor.scala
@@ -39,7 +39,7 @@ class AdActor extends Actor with DbCollections {
             ad_type_IMAGE,
             java.util.UUID.randomUUID.toString,
             java.util.UUID.randomUUID.toString,
-            Ad( u._1, u._2, "Short Headline Text about the Ad", None, Some(250), Some(300), None),
+            Ad( u._1, u._2, u._3, None, Some(250), Some(300), None),
             Math.random() * 100,
             Some(List("karedo.co.uk")),
             None,
@@ -55,17 +55,26 @@ class AdActor extends Actor with DbCollections {
     }
   }
 
-  def makeAdUrlPair: (String, String) = {
+  def makeAdUrlPair: (String, String, String) = {
 
     val ads = Array(
-      ("http://karedo.co.uk/ads/ad_300x250_1.jpg", "http://www.bbc.co.uk"),
-      ("http://karedo.co.uk/ads/ad_300x250_2.gif", "http://www.google.co.uk"),
-      ("http://karedo.co.uk/ads/ad_300x250_3.jpg", "http://www.yahoo.com"),
-      ("http://karedo.co.uk/ads/ad_300x250_4.jpg", "http://www.nytimes.com"),
-      ("http://karedo.co.uk/ads/ad_300x250_5.jpg", "http://www.audi.com"),
-      ("http://karedo.co.uk/ads/ad_300x250_6.jpg", "http://www.karedo.co.uk"),
-      ("http://karedo.co.uk/ads/ad_300x250_7.png", "http://www.guardian.com"),
-      ("http://karedo.co.uk/ads/ad_300x250_8.jpg", "http://www.times.com")
+      ("http://karedo.co.uk/ads/c1.jpg",
+        "https://www.washingtonpost.com/news/answer-sheet/wp/2015/03/26/no-finlands-schools-arent-giving-up-traditional-subjects-heres-what-the-reforms-will-really-do/",
+        "No, Finland isn’t ditching traditional school subjects. Here’s what’s really happening."),
+      ("http://karedo.co.uk/ads/c2.jpg", "http://www.iflscience.com/health-and-medicine/paralyzed-monkeys-walk-again-thanks-brain-implant/",
+        "Paralyzed Monkeys Walk Again Thanks To Brain Implant"),
+      ("http://karedo.co.uk/ads/c3.jpg",
+        "http://www.financeworld.news/index.php/2016/11/02/rich-bankers-terrified/",
+        "Rich Bankers Are Terrified Their Secret Weapon Is Finally Revealed"),
+      ("http://karedo.co.uk/ads/c4.jpg", "http://www.nytimes.com/2016/11/12/business/economy/donald-trump-trade-tpp-trans-pacific-partnership.html",
+        "What Is Lost by Burying the Trans-Pacific Partnership?"),
+      ("http://karedo.co.uk/ads/c5.jpg", "http://www.bbc.co.uk/news/uk-37951771", "Armistice Day in pictures"),
+      ("http://karedo.co.uk/ads/c6.jpg", "http://www.forbes.com/sites/kathleenchaykowski/2016/11/10/mark-zuckerberg-says-fake-news-on-facebook-did-not-sway-the-u-s-election/#4415191146a5",
+        "Mark Zuckerberg Says Fake News On Facebook Did Not Sway The U.S. Election"),
+      ("http://karedo.co.uk/ads/c7.png", "https://www.theguardian.com/sport/blog/2016/nov/10/conor-mcgregor-eddie-alvarez-ufc-205",
+        "The cheapening of Conor McGregor's once-clever act"),
+      ("http://karedo.co.uk/ads/c8.jpg", "http://www.nytimes.com/2016/11/12/technology/10-second-videos-from-your-sunglasses-thank-snapchat.html?ref=technology",
+        "10-Second Videos From Your Sunglasses. Thank Snapchat.")
     )
 
     val i:Int = (Math.random() * ads.length).toInt
