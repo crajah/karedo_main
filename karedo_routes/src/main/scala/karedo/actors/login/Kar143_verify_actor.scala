474dfa7b4239543c9d7e8f22dc603ca3d1d07e1a
Chandan Rajah <crajah@parallelai.com>
2016-11-11 00:47:20 +0000

    Small bug fixes to email verify

--- karedo_routes/src/main/scala/karedo/actors/login/Kar143_verify_actor.scala
+++ karedo_routes/src/main/scala/karedo/actors/login/Kar143_verify_actor.scala
@@ -66,7 +66,7 @@ trait Kar143_verify_actor
       }
     } match {
       case Success(s) => s
-      case Failure(f) => OK(APIResponse(welcome.html.email_verify_error.render(f.toString).toString, HTTP_OK_200, MIME_HTML))
+      case Failure(f) => OK(APIResponse(welcome.html.email_verify_error.render(f.toString + "<br>" + f.getStackTrace.map(_.toString).reduce(_ + "<br>" + _)).toString, HTTP_OK_200, MIME_HTML))
     }
   }
 }
