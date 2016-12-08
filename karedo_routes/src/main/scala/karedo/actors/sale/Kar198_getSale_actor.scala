de5a7ce3d56aec85b30fbf951e3a1fb4edc6abe2
Chandan Rajah <crajah@parallelai.com>
2016-12-08 23:20:23 +0000

    Fixed GET /sale bug

--- karedo_routes/src/main/scala/karedo/actors/sale/Kar198_getSale_actor.scala
+++ karedo_routes/src/main/scala/karedo/actors/sale/Kar198_getSale_actor.scala
@@ -33,9 +33,11 @@ trait Kar198_getSale_actor
         Try[Result[Error, APIResponse]] {
           val sale = dbSale.find(saleId).get
 
+          val newSale = sale.copy(karedos = karedos_to_appKaredos(sale.karedos).toLong)
+
 //          if( sale.receiver_id != accountId) OK(APIResponse("Conflict", HTTP_CONFLICT_409))
 //          else
-          OK(APIResponse(sale.toJson.toString, HTTP_OK_200))
+          OK(APIResponse(newSale.toJson.toString, code))
         } match {
           case Success(s) => s
           case Failure(f) => MAKE_THROWN_ERROR(f)
