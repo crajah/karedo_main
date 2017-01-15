2b2fc768477a22f661c95788facc17a64f5b41a8
Chandan Rajah <crajah@karedo.co.uk>
2017-01-15 21:01:36 +0000

    Added minor bug fix to tests

--- karedo_routes/src/test/scala/common/AllTests.scala
+++ karedo_routes/src/test/scala/common/AllTests.scala
@@ -2,7 +2,7 @@ package common
 
 import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import karedo.entity.{UserAccount, UserApp, UserKaredos}
-import karedo.entity.dao.{DbMongoDAO, MongoConnection}
+import karedo.entity.dao.{DbMongoDAO1, MongoConnection1}
 import karedo.routes.Routes
 import karedo.util._
 import org.scalatest.{Matchers, WordSpec}
@@ -15,7 +15,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * Created by pakkio on 10/21/16.
   */
 trait AllTests extends WordSpec
-  with MongoConnection
+  with MongoConnection1
   with Routes
   with ScalatestRouteTest
   with DbCollections
@@ -27,6 +27,6 @@ trait AllTests extends WordSpec
   implicit val timeout: RouteTestTimeout = RouteTestTimeout(1000.second(span))
 
   // can't clear everything otherwise tests cannot go in parallel (!)
-  DbMongoDAO.tablePrefix = "TestRoutes_"
+  DbMongoDAO1.tablePrefix = "TestRoutes_"
 
 }
