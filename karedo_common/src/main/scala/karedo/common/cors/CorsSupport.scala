dad2486d28d923dbd5e8e86b593b52f8bb10f82d
Chandan Rajah <crajah@karedo.co.uk>
2017-05-31 23:51:01 +0100

    Postman Collection and Environments

---
+++ karedo_common/src/main/scala/karedo/common/cors/CorsSupport.scala
@@ -0,0 +1,8 @@
+package karedo.common.cors
+
+/**
+  * Created by charaj on 20/05/2017.
+  */
+trait CorsSupport {
+
+}
---
+++ karedo_common/src/main/scala/karedo/common/jwt/JWTDirectives.scala
@@ -0,0 +1,8 @@
+package karedo.common.jwt
+
+/**
+  * Created by charaj on 20/05/2017.
+  */
+class JWTDirectives {
+
+}
